418b7d1e943c3db03006797ea235a62a09ba9bfc
Caio Colaiacovo <caiohshot@gmail.com>
2017-11-13 21:32:13 -0300

    Adding some resources and html

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import avatar from './avatar.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="top">
+            <input type="text" placeholder="Buscar"/>
+        </div>
+        <div className="app">
+            <div className="post">
+                <div className="header">
+                    <img src={avatar} alt="" className="avatar"></img>
+                    <div className="avatar-name">caiocolaiacovo</div>
+                </div>
+                <div className="photo">
+                    <img src="https://iso.500px.com/wp-content/uploads/2016/11/stock-photo-159533631-1500x1000.jpg" />
+                </div>
+                <div className="interaction">
+                    <i className="fa fa-heart-o fa-lg" aria-hidden="true"></i>
+                    <i className="fa fa-comment-o fa-lg" aria-hidden="true"></i>
+                    <div className="likes-quantity">10 curtidas</div>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
