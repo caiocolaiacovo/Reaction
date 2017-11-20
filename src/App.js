229fb4726bd85e8ad7a75020eb9f1c51a84b4694
Caio Colaiacovo <caiohshot@gmail.com>
2017-11-19 21:12:47 -0300

    More layout improvements

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
             <div className="moment">
                 <div className="moment-header">
                     <img src={avatar} alt="" className="avatar-picture"></img>
-                    <div className="avatar-name">caiocolaiacovo</div>
+                    <div className="avatar-name"><a href="#">caiocolaiacovo</a></div>
                 </div>
                 <div className="moment-picture">
                     {/*<img src="https://iso.500px.com/wp-content/uploads/2016/11/stock-photo-159533631-1500x1000.jpg"></img>*/}
@@ -21,9 +21,23 @@ class App extends Component {
                     <img src="https://i1.wp.com/www.k9magazine.com/wp-content/uploads/YuCALM_Dog_Master1_Print.jpg?resize=1000%2C600"></img>
                 </div>
                 <div className="interaction">
-                    <i className="fa fa-heart-o fa-lg" aria-hidden="true"></i>
-                    <i className="fa fa-comment-o fa-lg" aria-hidden="true"></i>
-                    <div className="likes-quantity">10 reactions</div>
+                    <div className="interaction-icons">
+                        <i className="fa fa-heart-o fa-lg" aria-hidden="true"></i>
+                        <i className="fa fa-comment-o fa-lg" aria-hidden="true"></i>
+                        <i className="fa fa-ellipsis-h fa-lg" aria-hidden="true"></i>
+                    </div>
+                    <div className="interaction-reactions">
+                        <a href="#">10 reactions</a>
+                    </div>
+                    <div className="interaction-comments">
+                        <ul>
+                            <li><a href="#">nome</a> teste</li>
+                            <li><a href="#">nome</a> teste</li>
+                        </ul>
+                    </div>
+                    <div className="interaction-elapsed-time">
+                        HÁ 15 MINUTOS
+                    </div>
                 </div>
             </div>
         </div>
