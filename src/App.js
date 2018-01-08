fa797d3fd1632f9acee959f979b42cc287967e47
Caio Colaiacovo <caiohshot@gmail.com>
2018-01-07 21:42:08 -0300

    Adding reactions popover

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ class App extends Component {
       <div>
         <Header/>
         <Moments/>
+        <div className="popover">
+          <div className="background"></div>
+          <div className="reactions">
+            <img src={process.env.PUBLIC_URL + '/emoticons/laughing.png'} alt="teste"></img>
+            <img src={process.env.PUBLIC_URL + '/emoticons/in-love.png'} alt="teste"></img>
+            <img src={process.env.PUBLIC_URL + '/emoticons/crying.png'} alt="teste"></img>
+            <img src={process.env.PUBLIC_URL + '/emoticons/rich.png'} alt="teste"></img>
+            <img src={process.env.PUBLIC_URL + '/emoticons/angry.png'} alt="teste"></img>
+          </div>
+        </div>
       </div>
     );
   }
